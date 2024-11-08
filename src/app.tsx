4efdf5f32caea7393ef4ad66d88f94d4ea294d7f
cordec <116736965+mbrooke14@users.noreply.github.com>
2024-11-07 23:15:28 -0500

    Added more statistics

--- src/app.tsx
+++ src/app.tsx
@@ -11,14 +11,21 @@ export default function App() {
 
         <main>
             <CardGrid>
-                <Card scalar={1}                    unit="secondes" precision="1" cardname="Temps Écoulé" />
-                <Card scalar={1377 * 0.91}          unit="tonnes"   precision="0" cardname="CO₂ Globale"  />
-                <Card scalar={1377 * 0.91 * 0.0151} unit="tonnes"   precision="0" cardname="CO₂ Canada"   />
+                <Card scalar={1} unit="secondes" precision="1" cardname="Temps Écoulé" id="time" />
+
+                <Card scalar={1377 * 0.91}                          unit="tonnes"   precision="0" cardname="CO₂ Globale"  />
+                <Card scalar={1377 * 0.91 * 0.0151}                 unit="tonnes"   precision="0" cardname="CO₂ Canada"   />
+                <Card scalar={1377 * 0.91 * 0.0151 / 8_000_000_000} unit="tonnes"   precision="7" cardname="CO₂ par personne (mondiale)"   />
+
+                <Card scalar={1377 * 0.91 * 50}                  unit="USD" precision="0" cardname="Cout Globale" />
+                <Card scalar={1377 * 0.91 * 0.0151 * 65}         unit="CAD" precision="0" cardname="Cout au Canada" />
+                <Card scalar={1377 * 0.91 * 50 / 8_000_000_000 } unit="USD" precision="5" cardname="Cout par personne (mondiale)" />
             </CardGrid>
         </main>
 
         <footer>
             <p>Yann-Emmanuel Akadan et Matthew Brookes</p>
+            <p>**Ceci est une estimation qui prends en considération des quantitées moyennes.**</p>
         </footer>
     </div>
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef } from "react";
 
 import "./card.css"
 
-export default function Card({ scalar, unit, precision, cardname, ...props }: {[key: string]: any}) {
+export default function Card({ scalar, unit, precision, cardname, id }: {[key: string]: any}) {
     const reference = useRef(null)
 
     const update = function (ms) {
@@ -14,7 +14,7 @@ export default function Card({ scalar, unit, precision, cardname, ...props }: {[
 
     useEffect(update, [])
 
-    return <div className="card" {...props} >
+    return <div className="card" id={id} >
         <h2 className="cardname">{cardname}</h2>
         <h3 className="cardvalue" ref={reference}>0</h3>
         <p className="cardunit">{unit}</p>
