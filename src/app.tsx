6c0fb0df8aa282f307fb2f4fa5053dda4b4dcfa7
cordec <116736965+mbrooke14@users.noreply.github.com>
2024-11-07 22:05:44 -0500

    Improved mobile experience

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,14 @@ import Card from "./components/card";
 import "./app.css"
 
 export default function App() {
-	return <>
+	return <div id="content">
         <header>
             <h1 id="title">Émissions de CO₂ — Combustibles Fossiles</h1>
         </header>
 
         <main>
             <CardGrid>
-                <Card scalar={1}                    unit="secondes" precision="1" cardname="Temps écoulé" />
+                <Card scalar={1}                    unit="secondes" precision="1" cardname="Temps Écoulé" />
                 <Card scalar={1377 * 0.91}          unit="tonnes"   precision="0" cardname="CO₂ Globale"  />
                 <Card scalar={1377 * 0.91 * 0.0151} unit="tonnes"   precision="0" cardname="CO₂ Canada"   />
             </CardGrid>
@@ -20,5 +20,5 @@ export default function App() {
         <footer>
             <p>Yann-Emmanuel Akadan et Matthew Brookes</p>
         </footer>
-    </>
+    </div>
 }
