de052f26b12acfadf99e6ec87865f4da6b2bb3ff
cordec <116736965+mbrooke14@users.noreply.github.com>
2024-11-07 23:30:35 -0500

    Improved readability of values

--- src/app.tsx
+++ src/app.tsx
@@ -3,6 +3,16 @@ import Card from "./components/card";
 
 import "./app.css"
 
+const POP = 8_000_000_000
+
+const TON_ANNUAL = 35_000_000_000
+const TON_SECOND = TON_ANNUAL / 365 / 24 / 60 / 60
+
+const CAN_CONTRB = 0.0151
+
+const AVR_COST = 50 // USD
+const CAN_COST = 65 // CAD
+
 export default function App() {
 	return <div id="content">
         <header>
@@ -11,13 +21,13 @@ export default function App() {
 
         <main>
             <CardGrid>
-                <Card scalar={1377 * 0.91}                                 unit="tonnes"  precision="0" cardname="CO₂ Globale"  />
-                <Card scalar={1377 * 0.91 * 0.0151}                        unit="tonnes"  precision="0" cardname="CO₂ Canada"   />
-                <Card scalar={1377 * 0.91 * 0.0151 / 8_000_000_000 * 1000} unit="kg"      precision="7" cardname="CO₂ par personne (mondiale)"   />
+                <Card scalar={TON_SECOND}                           unit="tonnes" precision="0" cardname="CO₂ Globale"  />
+                <Card scalar={TON_SECOND * CAN_CONTRB}              unit="tonnes" precision="0" cardname="CO₂ Canada"   />
+                <Card scalar={TON_SECOND * CAN_CONTRB / POP * 1000} unit="kg"     precision="7" cardname="CO₂ par personne (mondiale)"   />
 
-                <Card scalar={1377 * 0.91 * 50}                  unit="USD" precision="0" cardname="Cout Globale" />
-                <Card scalar={1377 * 0.91 * 0.0151 * 65}         unit="CAD" precision="0" cardname="Cout au Canada" />
-                <Card scalar={1377 * 0.91 * 50 / 8_000_000_000 } unit="USD" precision="5" cardname="Cout par personne (mondiale)" />
+                <Card scalar={TON_SECOND * AVR_COST}              unit="USD" precision="0" cardname="Cout Globale" />
+                <Card scalar={TON_SECOND * CAN_CONTRB * CAN_COST} unit="CAD" precision="0" cardname="Cout au Canada" />
+                <Card scalar={TON_SECOND * AVR_COST / POP}        unit="USD" precision="5" cardname="Cout par personne (mondiale)" />
 
                 <Card scalar={1} unit="secondes" precision="1" cardname="Temps Écoulé" id="time" />
             </CardGrid>
